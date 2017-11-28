322cb3c421938fbdc781aee468d06471f7d25bc3
RinsibleElk <oliver.samson@gmail.com>
2017-11-28 19:26:11 +0000

    Start making tests for flashcard session

---
+++ TheHerbalEscape/TheHerbalEscapeTests/FlashcardSessionTests.swift
@@ -0,0 +1,28 @@
+//
+//  FlashcardSessionTests.swift
+//  TheHerbalEscapeTests
+//
+//  Created by Oliver Samson on 27/11/2017.
+//  Copyright © 2017 Oliver Samson. All rights reserved.
+//
+
+import XCTest
+@testable import TheHerbalEscape
+
+class FlashcardSessionTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testCreateFirstEverSession() {
+        // Create a fake content repo, initialize an empty fake data controller.
+        // Create a flashcard session.
+        // Check the chosen cards.
+    }
+}
